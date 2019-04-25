e6387d6b6e0754e983c44759e341ebca57b1b407
mf226 <cph-mf226@cphbusiness.dk>
2019-04-25 22:38:10 +0200

    added REST endpoint api/generic/get/{id}

---
+++ src/main/java/rest/GenericResource.java
@@ -0,0 +1,67 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import utils.SWFetcher;
+
+/**
+ * REST Web Service
+ *
+ * @author Fen
+ */
+@Path("generic")
+public class GenericResource {
+
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("get/{id}")
+    @RolesAllowed("user")
+    public Response getOne(@PathParam("id") int id) {
+            SWFetcher fetcher = new SWFetcher();
+        try {
+            String res = fetcher.getSwappiData(id);
+            String result = (gson.toJson(res));
+            return Response.ok().entity(result).build();
+        } catch (IOException ex) {
+            Logger.getLogger(GenericResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.serverError().build();
+        }
+    }
+
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("getAll")
+//    @RolesAllowed("user")
+//    public String getFromUser() {
+//
+//        List<String> res = parallel.ParallelPinger.getStatusFromAllServers();
+//        String result = ("{\"Star wars stuff\":\"" + gson.toJson(res) + "\"}");
+//        return result;
+//    }
+}
--- src/main/resources/META-INF/testdata.sql
+++ src/main/resources/META-INF/testdata.sql
@@ -9,5 +9,5 @@ INSERT INTO users (user_name, user_pass) VALUES ('user', '$2a$10$mSZjeQg//BMfLL4
 INSERT INTO users (user_name, user_pass) VALUES ('admin', '$2a$10$E1GGASKVGmZvQK9Pkyh4sObxHR3x1hnpp7qTQwJDotDBOWZwBLy7a');
 INSERT INTO roles (role_name) VALUES ('user');
 INSERT INTO roles (role_name) VALUES ('admin');
-INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'admin');
+INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'user');
 INSERT INTO user_roles (user_name, role_name) VALUES ('admin', 'admin');
